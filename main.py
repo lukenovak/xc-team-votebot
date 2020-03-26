710a2e406c62a833e1b5435da959befd9b1e9238
Luke Novak <luken22@optonline.net>
2020-03-26 19:35:42 -0400

    inital commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from time import sleep
+
+#do this through chrome
+driver = webdriver.Firefox()
+
+#get the page
+driver.get('https://docs.google.com/forms/d/e/1FAIpQLSdscFnPYPSN5dhf6_HftZFcXW9kPZ68ANVZmE96uvIyoKBGQQ/viewform')
+
+while True:
+    vote_btn = driver.find_element_by_xpath('//div[@aria-label="Men\'s Cross Country - 1-2 Punch Earns Runner-up Finish"]')
+    vote_btn.click()
+
+    submit_button = driver.find_elements_by_class_name("freebirdFormviewerViewNavigationSubmitButton")
+    submit_button[0].click()
+
+    reload_button = driver.find_element_by_xpath("//*[text()='Submit another response']")
+    reload_button.click()
+    sleep(0.25)
