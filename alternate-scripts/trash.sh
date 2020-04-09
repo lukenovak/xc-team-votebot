9f7a25aad8efb7d743eff61be3df00ce58463167
Luke Novak <contact@nluken.com>
2020-04-09 19:26:20 -0400

    updated script

--- alternate-scripts/trash.sh
+++ alternate-scripts/trash.sh
@@ -1,4 +1,4 @@
 while true
 do
-	curl -d "entry.890652681=Men\'s+Cross+Country+-+1-2+Punch+Earns+Runner-up+Finish" https://docs.google.com/forms/u/0/d/e/1FAIpQLSdscFnPYPSN5dhf6_HftZFcXW9kPZ68ANVZmE96uvIyoKBGQQ/formResponse
+    curl 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSd8xTc3qCMny2MroWd_TVbVoZ79G3HYeDEgwCNFKoWXirGrqA/formResponse'  --data 'entry.807910780=Women%27s+Cross+Country+-+Wise+Runs+into+Record+Book&fvv=1&draftResponse=%5Bnull%2Cnull%2C%229098056647003986476%22%5D%0D%0A&pageHistory=0&fbzx=9098056647003986476'
 done
